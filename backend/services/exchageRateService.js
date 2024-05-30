0ffdcdc08fa5b5910d237473243ab8dd2a7abbb7
Kavindu Thilan <kavinduthilan2000@outlook.com>
2024-05-30 17:32:52 +0530

    exported the function

--- backend/services/exchageRateService.js
+++ backend/services/exchageRateService.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const getExchageRates = async (base) => {
+export const getExchangeRates = async (base) => {
     const response = await axios.get(`https://api.exchangerate-api.com/v4/latest/${base}`);
     return response.data;
 };
-
-export default getExchageRates;
