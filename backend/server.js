137eade8be5053039832be172c655067ed0e2bbb
Kavindu Thilan <kavinduthilan2000@outlook.com>
2024-05-30 17:31:48 +0530

    added endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import {connectDB} from './config/db.js';
-
+import exchangeRateRoutes from './routes/exchangeRateRoutes.js';
 
 connectDB();
 
 const app = express();
 
-app.use(express.json());
+app.use('/api/exchange-rates', exchangeRateRoutes);
 
 
 const port = process.env.PORT || 5000;
