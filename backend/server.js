e77fd019446160dec5183b34adf23e18b47a0a56
Kavindu Thilan <kavinduthilan2000@outlook.com>
2024-06-03 03:47:31 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import {connectDB} from './config/db.js';
 import exchangeRateRoutes from './routes/exchangeRateRoutes.js';
+import cors from 'cors';
 
 connectDB();
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173' // Allow requests from this origin
+}));
 
 app.use('/api/exchange-rates', exchangeRateRoutes);
 
